2bf7665e13958b769049c9eab4b1320da998fdca
saoraozhe3hao <saoraozhe3hao@126.com>
2015-10-06 00:25:04 +0800

    sdf

--- phpDemo/main.php
+++ phpDemo/main.php
@@ -4,6 +4,10 @@
    require_once 'MyCtrl.class.php';
    require_once 'MyModel.class.php';
    require_once 'MyView.class.php';
+   //require与include的区别，当要加载的文件不存在时候, include会给一个warning警告, 然后继续运行
+   //而require则会给一个fatal error, 直接结束脚本
+   
+   
    
    //当前文件是MVC模式的入口文件，引入控制器、模型、视图
    //引入的所有类相当于在这个文件里定义的，因此类之间可以互相调用
